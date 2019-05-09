21a0e9dbaab5eb30ceb2f8fc953f1714f168814d
Rafał Nowakowski <gee@vp.pl>
2019-05-09 20:18:03 +0200

    Add babel test

--- script-compiled.js
+++ script-compiled.js
@@ -1,6 +1,25 @@
 'use strict';
 
 var sayHello = function sayHello() {
-  return alert('Hello world!');
+    return alert('Hello world!');
 };
 sayHello();
+
+var greeting = 'Hello User!';
+function greetWorld(isGreeting) {
+    if (isGreeting) {
+        var _greeting = 'Hello World!';
+        return _greeting;
+    }
+    return greeting;
+}
+greetWorld(false); // 'Hello User!'
+
+{
+    // rozpoczęcie bloku
+    var hello = 'test';
+} // zakończenie bloku
+
+function sayHelloTo(person) {
+    console.log('Hello, ' + person + ', nice to meet you!');
+}
--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 const sayHello = () => alert('Hello world!');
 sayHello();
+
+let greeting = 'Hello User!';
+function greetWorld(isGreeting) {
+    if (isGreeting) {
+        let greeting = 'Hello World!';
+        return greeting;
+    }
+    return greeting;
+}
+greetWorld(false); // 'Hello User!'
+
+{  // rozpoczęcie bloku
+    let hello = 'test';
+}  // zakończenie bloku
+
+function sayHelloTo(person) {
+    console.log(`Hello, ${person}, nice to meet you!`);
+}
